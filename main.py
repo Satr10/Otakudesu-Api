e3079ae3789dfec42e2e4b7562319cf886bada28
satrio <personal.satrio@protonmail.com>
2025-01-13 12:51:54 +0700

    feat: menambahkan endpoint untuk mendapatkan data anime
    Menambahkan endpoint baru untuk mendapatkan data anime, termasuk:
    
    - `/home` - menampilkan daftar anime yang sedang tayang dan telah selesai.
    - `/ongoing-anime` - menampilkan daftar anime yang sedang tayang.
    - `/completed-anime` - menampilkan daftar anime yang telah selesai.
    - `/search` - mencari anime berdasarkan judul.
    - `/anime/{slug}` - menampilkan detail informasi anime.
    - `/anime/{slug}/episodes` - menampilkan daftar episode anime.
    - `/episode/{episode_slug}` - menampilkan detail informasi episode.
    
    Perubahan ini meningkatkan fungsionalitas API dengan menyediakan akses ke berbagai data anime.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from typing import Optional
+from scrapers.main import *
+import asyncio
 
 app = FastAPI()
 
@@ -15,11 +17,34 @@ async def root():
 @app.get("/home")
 async def home():
     try:
+        airing, completed = await asyncio.gather(
+            scrape_airing_anime(), scrape_completed_anime()
+        )
         return {
             "status": "Ok",
             "data": {
-                "airing": {"0": "Title", "1": "Title"},
-                "completed": {"0": "Title", "1": "Title"},
+                "airing": [
+                    {
+                        "title": anime["title"],
+                        "episode": anime["episode"],
+                        "schedule": anime["schedule"],
+                        "date": anime["date"],
+                        "slug": anime["slug"],
+                        "url": anime["url"],
+                    }
+                    for anime in airing
+                ],
+                "completed": [
+                    {
+                        "title": anime["title"],
+                        "episode": anime["episode"],
+                        "rating": anime["rating"],
+                        "date": anime["date"],
+                        "slug": anime["slug"],
+                        "url": anime["url"],
+                    }
+                    for anime in completed
+                ],
             },
         }
     except Exception as e:
@@ -27,26 +52,46 @@ async def home():
 
 
 @app.get("/ongoing-anime")
-async def ongoing_anime(page: Optional[int] = None):
-    if page is None:
-        return {"status": "Error", "message": "Parameter 'page' dibutuhkan."}
+async def ongoing_anime(page: Optional[int] = 1):
     try:
+        airing = await scrape_airing_anime(page)
         return {
             "status": "Ok",
-            "data": {"0": "Title", "1": "Title"},
+            "data": [
+                {
+                    "title": anime["title"],
+                    "episode": anime["episode"],
+                    "schedule": anime["schedule"],
+                    "date": anime["date"],
+                    "slug": anime["slug"],
+                    "image": anime["image"],
+                    "url": anime["url"],
+                }
+                for anime in airing
+            ],
         }
     except Exception as e:
         return {"status": "Error", "message": str(e)}
 
 
 @app.get("/completed-anime")
-async def completed_anime(page: Optional[int] = None):
-    if page is None:
-        return {"status": "Error", "message": "Parameter 'page' dibutuhkan."}
+async def completed_anime(page: Optional[int] = 1):
     try:
+        completed = await scrape_completed_anime(page)
         return {
             "status": "Ok",
-            "data": {"0": "Title", "1": "Title"},
+            "data": [
+                {
+                    "title": anime["title"],
+                    "episode": anime["episode"],
+                    "rating": anime["rating"],
+                    "date": anime["date"],
+                    "slug": anime["slug"],
+                    "image": anime["image"],
+                    "url": anime["url"],
+                }
+                for anime in completed
+            ],
         }
     except Exception as e:
         return {"status": "Error", "message": str(e)}
@@ -55,9 +100,20 @@ async def completed_anime(page: Optional[int] = None):
 @app.get("/search")
 async def search(query: str):
     try:
+        result = await scrape_search_anime(query)
         return {
             "status": "Ok",
-            "data": {"Your Search: ": query},
+            "data": [
+                {
+                    "title": anime["title"],
+                    "status": anime["status"],
+                    "rating": anime["rating"],
+                    "slug": anime["slug"],
+                    "image": anime["image"],
+                    "url": anime["url"],
+                }
+                for anime in result
+            ],
         }
     except Exception as e:
         return {"status": "Error", "message": str(e)}
@@ -66,9 +122,25 @@ async def search(query: str):
 @app.get("/anime/{slug}")
 async def anime(slug: str):
     try:
+        data = await scrape_anime(slug)
         return {
             "status": "Ok",
-            "data": {"Anime: ": "Anime"},
+            "data": {
+                "title": data["title"],
+                "image": data["image"],
+                "japanese_title": data["japanese_title"],
+                "rating": data["rating"],
+                "producer": data["producer"],
+                "type": data["type"],
+                "status": data["status"],
+                "episode_total": data["episode_total"],
+                "duration": data["duration"],
+                "release_date": data["release_date"],
+                "studio": data["studio"],
+                "genre": data["genre"],
+                "episode_list": data["episode_list"],
+                "batch": data["batch"],
+            },
         }
     except Exception as e:
         return {"status": "Error", "message": str(e)}
@@ -77,31 +149,36 @@ async def anime(slug: str):
 @app.get("/anime/{slug}/episodes")
 async def episodes(slug: str):
     try:
+        data = await scrape_anime(slug)
         return {
             "status": "Ok",
-            "data": {"Episode: ": "Episode"},
+            "data": {
+                "episode_list": data["episode_list"],
+            },
         }
     except Exception as e:
         return {"status": "Error", "message": str(e)}
 
 
-@app.get("/anime/{slug}/episodes/{episode}")
-async def episode(slug: str, episode: str):
-    try:
-        return {
-            "status": "Ok",
-            "data": {"Episode: ": "Episode"},
-        }
-    except Exception as e:
-        return {"status": "Error", "message": str(e)}
+# tidak bisa diaplikasikan karna url setiap episode berbeda
+# @app.get("/anime/{slug}/episodes/{episode}")
+# async def episode(slug: str, episode: str):
+#     try:
+#         return {
+#             "status": "Ok",
+#             "data": {"Episode: ": "Episode"},
+#         }
+#     except Exception as e:
+#         return {"status": "Error", "message": str(e)}
 
 
-@app.get("/v1/episode/{episode}")
-async def episode(episode: str):
+@app.get("/episode/{episode_slug}")
+async def episode(episode_slug: str):
     try:
+        data = await scrape_single_episode(episode_slug)
         return {
             "status": "Ok",
-            "data": {"Episode: ": "Episode"},
+            "data": data,
         }
     except Exception as e:
         return {"status": "Error", "message": str(e)}
