cb817c1ceefb4f4abc5878a853f8c0e9a0082a1d
Satrio <personal.satrio@protonmail.com>
2025-05-28 18:28:52 +0700

    feat: add AnimeDetail and Episode types to enhance anime data structure

--- models/anime.go
+++ models/anime.go
@@ -11,3 +11,25 @@ type Anime struct {
 	Image    string  `json:"image"`
 	URL      string  `json:"url"`
 }
+
+type AnimeDetail struct {
+	Title         string    `json:"title"`
+	JapaneseTitle string    `json:"japaneseTitle,omitempty"`
+	Rating        string    `json:"rating,omitempty"`
+	Producer      string    `json:"producer,omitempty"`
+	Type          string    `json:"type,omitempty"` // anime quality type i.e BD or WEBDL
+	Status        string    `json:"status,omitempty"`
+	EpisodeTotal  string    `json:"episodeTotal,omitempty"`
+	Duration      string    `json:"duration,omitempty"`
+	ReleaseDate   string    `json:"releaseDate,omitempty"`
+	Studio        string    `json:"studio,omitempty"`
+	Genre         string    `json:"genre,omitempty"`
+	Synopsis      string    `json:"synopsis"`
+	Episodes      []Episode `json:"episodes,omitempty"`
+}
+
+type Episode struct {
+	EpisodeTitle string `json:"episodeTitle"`
+	Slug         string `json:"slug"`
+	URL          string `json:"url"`
+}
