ae19f637fa0719ca781ebed58a018aa061600178
Satrio <personal.satrio@protonmail.com>
2025-05-28 17:24:08 +0700

    fix: restore Status field in Anime model for proper data representation

--- models/anime.go
+++ models/anime.go
@@ -3,6 +3,7 @@ package models
 type Anime struct {
 	Title    string  `json:"title"`
 	Episode  string  `json:"episode"`
+	Status   string  `json:"status"`
 	Schedule *string `json:"schedule"`
 	Rating   *string `json:"rating"`
 	Date     string  `json:"date"`
