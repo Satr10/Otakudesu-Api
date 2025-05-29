a74e33b983866d3bfefa66ba7ef2c04d71b78a68
Satrio <personal.satrio@protonmail.com>
2025-05-29 13:28:52 +0700

    fix: add cache middleware to improve response performance

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/Satr10/Otakudesu-Api/router"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 )
 
 func main() {
 	app := fiber.New()
-
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("noCache") == "true"
+		},
+		Expiration:   1 * time.Minute,
+		CacheControl: true,
+	}))
 	router.InitRouter(app)
 
 	log.Fatal(app.Listen(":5001"))
